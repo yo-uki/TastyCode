f040be7228df217b61dc54c5a004341151493cf1
Aleksandra <yo-uki@wp.pl>
2025-03-24 15:20:24 +0100

    init

---
+++ src/main/java/pl/youkidesign/TastyCode/TastyCodeApplication.java
@@ -0,0 +1,13 @@
+package pl.youkidesign.TastyCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/youkidesign/TastyCode/TastyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.youkidesign.TastyCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
